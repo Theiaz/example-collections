516f46bb663e6e3893a9c4df01902a672a5b30a1
Julian Schäfer <schaefer@synyx.de>
2022-07-22 11:32:43 +0200

    Add simple composable

---
+++ vue/composables/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useMouse } from "./composables/mouse";
+const { x, y } = useMouse();
+</script>
+
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+    <div>Mouse position is at: {{ x }}, {{ y }}</div>
+  </main>
+</template>
